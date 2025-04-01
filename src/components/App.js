a917b62e87d06c190a24cf5101d881d690ffc887
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-04-01 12:05:14 -0400

    project init

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
-import React from "react";
+import { useState, useEffect } from 'react';
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+const [listings, setListings ] = useState([])
+
+useEffect(() => {
+  fetchListings()
+},[])
+
+async function fetchListings() {
+  try {
+    const r = await fetch (`http://localhost:6001/listings`)
+    if(!r.ok) {
+      throw new Error("💥 Error");
+    }
+    const data= await r.json()
+    setListings(data)
+  }catch (error) {console.error(error);}
+}
+
   return (
     <div className="app">
       <Header />
